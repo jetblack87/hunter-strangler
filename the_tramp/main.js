6ac7720d1f8c6adf9333ca9febaea2ec656ac01a
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2013-07-04 00:21:31 -0400

    Added setup of jaws libs

--- the_tramp/main.js
+++ the_tramp/main.js
@@ -1,3 +1,17 @@
-function load() {
-    document.getElementById("tst").innerHTML="Goodbye, cruel world!";
+function Main() {
+    /* Called once. Put your one-time initializing here. */
+    this.setup = function() {
+    }
+    
+    /* Called each gametick. Put your gamelogic here. */
+    this.update = function() {
+    }
+
+    /* Called each gametick after update(). Put your drawing here. */
+    this.draw = function() {
+    }
+}
+
+window.onload = function() {
+    jaws.start(Main)
 }
